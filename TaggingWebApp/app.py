4540bd212859f9b021f62e8e6feeb1d3ae809292
Belen Saavedra <saavedrariosm@berea.edu>
2025-04-14 11:25:19 -0400

    added js file for RM

---
+++ TaggingWebApp/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('app.html')
+
+@app.route('/form', methods=['GET','POST'])
+def form():
+    return render_template('form.html')
+
+@app.route('/RM', methods = ['GET', 'POST'])
+def relationship_manager():
+    return render_template('relationship_manager.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
